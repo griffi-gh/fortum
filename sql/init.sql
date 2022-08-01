9446c9524744c0628d49b62deca7c0e3ed993d49
griffi-gh <prasol258@gmail.com>
2022-08-02 00:54:18 +0200

    fix init.sql

--- sql/init.sql
+++ sql/init.sql
@@ -1,8 +1,8 @@
-CREATE TABLE IF NOT EXISTS users {
+CREATE TABLE IF NOT EXISTS users (
   user_id serial PRIMARY KEY,
   username VARCHAR(15) UNIQUE NOT NULL,
   email VARCHAR(255) UNIQUE NOT NULL,
   password_hash VARCHAR NOT NULL,
   created_on TIMESTAMP NOT NULL,
-  last_activity TIMESTAMP NOT NULL,
-}
+  last_activity TIMESTAMP NOT NULL
+);
