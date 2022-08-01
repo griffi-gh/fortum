a5e4c74be8dd9b7ca5ae02be40d25d8edf5f6c97
griffi-gh <prasol258@gmail.com>
2022-08-02 00:35:07 +0200

    update

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ use regex::Regex;
 use crypto::scrypt::{scrypt_simple, ScryptParams};
 
 lazy_static! {
-  static ref EMAIL_REGEX: Regex = Regex::new(r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)").unwrap();
+  static ref EMAIL_REGEX: Regex = Regex::new(r"^([a-z0-9_+]([a-z0-9_+.]*[a-z0-9_+])?)@([a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,6})").unwrap();
   static ref USERNAME_REGEX: Regex = Regex::new(r"^[a-zA-Z0-9_-]{3,15}$").unwrap();
   static ref PASSWORD_REGEX: Regex = Regex::new(r"^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$").unwrap();
   static ref SCRYPT_PARAMS: ScryptParams = ScryptParams::new(14, 8, 1);
