d0b18d80f67d1703b0df95b6d06f3003439fc9e2
griffi-gh <prasol258@gmail.com>
2022-08-02 13:47:20 +0200

    a

--- sql/init.sql
+++ sql/init.sql
@@ -6,5 +6,13 @@ CREATE TABLE users (
   password_hash VARCHAR NOT NULL,
   created_on TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   last_activity TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  user_role role_type NOT NULL DEFAULT 'user',
+  user_role role_type NOT NULL DEFAULT 'user'
+);
+CREATE TABLE tokens (
+  token VARCHAR PRIMARY KEY,
+  user_id integer NOT NULL,
+  expiry TIMESTAMP NOT NULL DEFAULT now() + INTERVAL '1 year 3 hours 20 minutes',
+  CONSTRAINT FOREIGN KEY(user_id)
+    REFERENCES users(user_id)
+    ON DELETE CASCADE
 );
