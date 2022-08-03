6862368629b4cf5bae30799df3a1f8acb8e850e1
griffi-gh <prasol258@gmail.com>
2022-08-03 15:52:54 +0200

    aaa

--- src/endpoints/frontend/register.rs
+++ src/endpoints/frontend/register.rs
@@ -1,6 +1,6 @@
 use rocket_dyn_templates::{Template, context};
 
-#[get("/register?success")]
+#[get("/register/success")]
 pub fn register_success() -> Template {
   Template::render("register", context! {
     success: true
