f6cee06fcbd45dc94ce1a18f39aa205234f91d06
griffi-gh <prasol258@gmail.com>
2022-08-04 14:23:14 +0200

    add password repeat check

--- src/endpoints/register.rs
+++ src/endpoints/register.rs
@@ -24,10 +24,16 @@ pub struct RegisterData {
   email: String,
   username: String,
   password: String,
+  repeat_password: Option<String>,
 }
 
 #[post("/register", data = "<data>")]
 pub async fn post_register(data: Form<RegisterData>, db: Connection<MainDatabase>, cookies: &CookieJar<'_>) -> Redirect {
+  if let Some(repeat) = data.repeat_password.as_ref() {
+    if &data.password != repeat {
+      return Redirect::to(uri!(register(error = Some("Passwords don't match"))));
+    }
+  }
   match MainDatabase::register(db, &data.email, &data.username, &data.password).await {
     Ok(token) => {
       cookies.add_private(Cookie::build("auth", token).secure(true).finish());
