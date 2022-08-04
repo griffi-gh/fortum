dec91011264d1104c49cf83d6d8e68718fa753e8
griffi-gh <prasol258@gmail.com>
2022-08-05 01:23:42 +0200

    a

--- static/js/header.js
+++ static/js/header.js
@@ -1,12 +1,13 @@
 if (document.getElementById("header")) {
-  const ups = document.getElementById("user-profile-section");
-  const lob = document.getElementById("logout-button");
-  lob.addEventListener('click', event => {
-    event.stopPropagation();
-  })
-  ups.addEventListener('click', event => {
-    event.preventDefault();
-  });
+  // const ups = document.getElementById("user-profile-section");
+  // const lob = document.getElementById("logout-button");
+  // lob.addEventListener('click', event => {
+  //   event.stopPropagation();
+  // })
+  // ups.addEventListener('click', event => {
+  //   event.preventDefault();
+  //   event.stopPropagation();
+  // });
 } else {
   console.warn('No header???');
 }
