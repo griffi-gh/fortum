a3cfa9b695bec40e02482f1101f28a1d3548ed0e
griffi-gh <prasol258@gmail.com>
2022-08-06 16:21:40 +0200

    fix

--- src/endpoints/submit.rs
+++ src/endpoints/submit.rs
@@ -4,6 +4,7 @@ use rocket_db_pools::Connection;
 use rocket_dyn_templates::{Template, context};
 use crate::db::MainDatabase;
 use crate::common::{Authentication, TemplateVars};
+use crate::endpoints::login::rocket_uri_macro_login;
 
 #[derive(FromForm)]
 pub struct PostSubmitData<'a> {
@@ -19,7 +20,7 @@ pub async fn submit(error: Option<&str>, vars: TemplateVars) -> Template {
 
 #[post("/submit", data = "<data>")]
 pub async fn submit_post(data: Form<PostSubmitData<'_>>, db: Connection<MainDatabase>, auth: Authentication) -> Redirect {
-  if !auth.valid { return Redirect::to("/login?error=Log in to post stuff"); }
+  if !auth.valid { return Redirect::to(uri!(login(error = Some("Log in before postring stuff")))); }
   match MainDatabase::submit_post(db, Some(auth.user_id.unwrap()), data.topic, &data.title, data.body).await {
     Ok(_) => Redirect::to(uri!("/post/todo_insert_id_here?success")),
     Err(err) => Redirect::to(uri!(submit(error = Some(err)))),
