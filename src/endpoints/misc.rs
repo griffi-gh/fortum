6d8728b925dc212a2961e9b2185b7971f2e4f274
griffi-gh <prasol258@gmail.com>
2022-08-18 00:21:45 +0200

    WHAT THE FUCK

--- src/endpoints/misc.rs
+++ src/endpoints/misc.rs
@@ -1,8 +1,8 @@
-macro_rules! define_handler {
+ macro_rules! define_handler {
   ($fname: tt, $route: literal, $template: literal) => {
     #[get($route)]
-    pub fn $fname(vars: crate::common::TemplateVars) -> rocket_dyn_templates::Template {
-      rocket_dyn_templates::Template::render($template, rocket_dyn_templates::context!{vars})
+    pub fn $fname(vars: crate::common::TemplateVars) -> (::rocket::http::ContentType, ::rocket_dyn_templates::Template) {
+      (::rocket::http::ContentType::HTML, ::rocket_dyn_templates::Template::render($template, ::rocket_dyn_templates::context!{vars}))
     }
   };
 }
